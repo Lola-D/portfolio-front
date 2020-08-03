795e838eeac1d12bb1fc27f38ee6a8705aef1569
Lola <lola.donval@gmail.com>
2020-08-03 16:06:34 +0200

    add data to project page

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,31 +5,15 @@ import './Card.css'
 
 const Card = ({ project }) => {
   return (
-    <Link to={`/projects/${project.id}`}>
-      <div className='card'>
-        <h2 className='card-title'>{project.name}</h2>
-        <img
-          className='card-image'
-          src='../previews/julian-wild.png'
-          alt='preview of  project'
-        />
-        <p className='card-text'>{project.description}</p>
-        <a
-          href={project.github_url}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Lien Github
-        </a>
-        <a
-          href={project.project_url}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Lien du site
-        </a>
-      </div>
-    </Link>
+    <div className='card'>
+      <h2 className='card-title'>{project.name}</h2>
+      <img
+        className='card-image'
+        src='../previews/julian-wild.png'
+        alt='preview of  project'
+      />
+      <Link to={`/projects/${project.id}`}>Voir plus</Link>
+    </div>
   )
 }
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Project = () => {
-  return (
-    <div>
-      <p>Hello</p>
-    </div>
-  )
+import axios from 'axios'
+
+class Project extends Component {
+
+  state = {
+    project: []
+  }
+
+  componentDidMount() {
+    this.handleProject()
+  }
+
+  handleProject = () => {
+    const id = this.props.match.params.id
+    axios
+      .get(`${process.env.REACT_APP_SERVER_URL}/project/${id}`)
+      .then(res => this.setState({ project: res.data }))
+  }
+
+  render() {
+    const { project } = this.state
+    return (
+      <div>
+        <h1>{project.name}</h1>
+        <img src='' alt='' />
+        <p>{project.description}</p>
+        <a
+          href={project.github_url}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Lien vers Github
+        </a>
+        <a
+          href={project.project_url}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Lien vers le Site
+        </a>
+      </div>
+    )
+
+  }
 }
 
 export default Project
