1987e500ec1494d69bd70a7e9a2ff8bbaa27e4f7
Lola <lola.donval@gmail.com>
2020-08-03 15:39:42 +0200

    try to  display image and add link to card

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
-// import Project from './components/Project'
+import Project from './components/Project'
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path='/' component={Home} />
-        {/* <Route path='/projects/:id' component={Project} /> */}
+        <Route path='/projects/:id' component={Project} />
       </Switch>
     </Router>
   )
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,28 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import './Card.css'
 
 const Card = ({ project }) => {
   return (
-    <div className='card'>
-      <h2 className='card-title'>{project.name}</h2>
-      <img className='card-image' src='../previews/julian-wild.png' />
-      <p className='card-text'>{project.description}</p>
-      <a
-        href={project.github_url}
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        Lien Github
-      </a>
-      <a
-        href={project.project_url}
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        Lien du site
-      </a>
-    </div>
+    <Link to={`/projects/${project.id}`}>
+      <div className='card'>
+        <h2 className='card-title'>{project.name}</h2>
+        <img
+          className='card-image'
+          src='../previews/julian-wild.png'
+          alt='preview of  project'
+        />
+        <p className='card-text'>{project.description}</p>
+        <a
+          href={project.github_url}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Lien Github
+        </a>
+        <a
+          href={project.project_url}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Lien du site
+        </a>
+      </div>
+    </Link>
   )
 }
 
