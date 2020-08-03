c8c8460e6622cc453262d6c7977806473d2d9d1f
Lola <lola.donval@gmail.com>
2020-08-03 13:51:41 +0200

    get all projects from database

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Home extends Component {
+
+  componentDidMount() {
+    this.getProjects()
+  }
+
+  getProjects = () => {
+    axios
+      .get(`${process.env.REACT_APP_SERVER_URL}/home`)
+      .then(res => console.log(res))
+  }
+
   render() {
     return (
       <div>
